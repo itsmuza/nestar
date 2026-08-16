b7916575ae35ef47f9f6acc0012a68f47bf2469b
Muzaffar <itsmuuza@icloud.com>
2026-08-17 00:01:45 +0900

    feat: develop global GraphQL orror handling system

--- apps/nestar/src/app.module.ts
+++ apps/nestar/src/app.module.ts
@@ -7,6 +7,7 @@ import { ApolloDriver } from '@nestjs/apollo';
 import { AppResolver } from './app.resolver';
 import { ComponentsModule } from './components/components.module';
 import { DatabaseModule } from './database/database.module';
+import { T } from './libs/types/common';
 
 @Module({
 	imports: [
@@ -15,6 +16,16 @@ import { DatabaseModule } from './database/database.module';
 			driver: ApolloDriver,
 			uploads: false,
 			autoSchemaFile: true,
+			formatError: (error: T) => {
+				console.log('error:', error);
+				const graphQLFormattedError = {
+					code: error?.extensions.code,
+					message:
+						error?.extensions?.exception?.response?.message || error?.extension?.response?.message || error?.message,
+				};
+				console.log('GRAPHQL GLOBAL ERR:', graphQLFormattedError);
+				return graphQLFormattedError;
+			},
 		}),
 		ComponentsModule,
 		DatabaseModule,
