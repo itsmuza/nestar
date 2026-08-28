d799337b2182390ccd07421cd5d52dbcb679b179
Muzaffar <itsmuuza@icloud.com>
2026-08-28 19:50:00 +0900

    feat: develop createProperty graphql api

--- apps/nestar/src/components/member/member.service.ts
+++ apps/nestar/src/components/member/member.service.ts
@@ -8,7 +8,7 @@ import { MemberStatus, MemberType } from '../../libs/enums/member.enum';
 import { Direction, Message } from '../../libs/enums/common.enum';
 import { AuthService } from '../auth/auth.service';
 import { MemberUpdate } from '../../libs/dto/member/member.update';
-import { T } from '../../libs/types/common';
+import { StatisticModifier, T } from '../../libs/types/common';
 import { ViewService } from '../view/view.service';
 import { ViewGroup } from '../../libs/enums/view.enum';
 
@@ -144,6 +144,24 @@ export class MemberService {
 
 		if (!result) throw new InternalServerErrorException(Message.UPDATE_FAILED);
 
+		return result;
+	}
+
+	async memberStatsEditor(input: StatisticModifier): Promise<Member> {
+		const { _id, targetKey, modifier } = input;
+		const result = await this.memberModel
+			.findByIdAndUpdate(
+				_id,
+				{
+					$inc: { [targetKey]: modifier },
+				},
+				{ new: true },
+			)
+			.exec();
+
+		if (!result) {
+			throw new InternalServerErrorException(Message.UPDATE_FAILED);
+		}
 		return result;
 	}
 }
--- apps/nestar/src/libs/types/common.ts
+++ apps/nestar/src/libs/types/common.ts
@@ -1,3 +1,11 @@
+import { ObjectId } from 'bson';
+
 export interface T {
 	[key: string]: any;
+}
+
+export interface StatisticModifier {
+	_id: ObjectId;
+	targetKey: string;
+	modifier: number;
 }
