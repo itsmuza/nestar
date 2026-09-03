fc24b663f2078a48497b1c01f517db38e0b5a9cf
Muzaffar <itsmuuza@icloud.com>
2026-09-03 14:27:27 +0900

    feat: configure comment module and create comment related resolver and service module

--- apps/nestar/src/app.module.ts
+++ apps/nestar/src/app.module.ts
@@ -8,6 +8,7 @@ import { AppResolver } from './app.resolver';
 import { ComponentsModule } from './components/components.module';
 import { DatabaseModule } from './database/database.module';
 import { T } from './libs/types/common';
+import { CommentService } from './component/comment/comment.service';
 
 @Module({
 	imports: [
@@ -31,6 +32,6 @@ import { T } from './libs/types/common';
 		DatabaseModule,
 	],
 	controllers: [AppController],
-	providers: [AppService, AppResolver],
+	providers: [AppService, AppResolver, CommentService],
 })
 export class AppModule {}
