569752465fde8f85e9c48e3b86bdcc56a476e7cb
Muzaffar <itsmuuza@icloud.com>
2026-09-08 23:15:25 +0900

    feat: create WebSocket Connection process

--- apps/nestar/src/components/view/view.service.ts
+++ apps/nestar/src/components/view/view.service.ts
@@ -59,6 +59,8 @@ export class ViewService {
 			])
 			.exec();
 
+		console.log('data:', data);
+
 		const result: Properties = { list: [], metaCounter: data[0].metaCounter };
 		result.list = data[0].list.map((ele) => ele.visitedProperty);
 
